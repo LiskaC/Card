a8bcc479325b789879a1a956312305f795a0109a
Yelizaveta Crofts <yscrofts@hotmail.com>
2018-11-26 23:28:38 +0100

    drew interior background, created back var

--- script.js
+++ script.js
@@ -4,29 +4,39 @@ window.onload = function () {
   var ctx = canvas.getContext("2d");
   var cardFront = new Image();
   cardFront.src = 'images/cardFront.jpg';
+  var cardBack = new Image();
+  cardBack.src = "#"; //remember to upload
 
-  cardFrontHeight = 600;
-  cardFrontWidth = cardFrontHeight * (1921 / 2704);
-  cardX = ((canvas.width - cardFrontWidth) / 2);
+  cardHeight = 600;
+  cardWidth = cardHeight * (1921 / 2704);
+  cardX = ((canvas.width - cardWidth) / 2);
   cardY = 50;
 
 
   function drawCardFront() {
 
     cardFront.onload = function () {
-      ctx.drawImage(cardFront, cardX, cardY, cardFrontWidth, cardFrontHeight);
+      ctx.drawImage(cardFront, cardX, cardY, cardWidth, cardHeight);
     };
   };
   drawCardFront();
+
+  function drawCardBack() {
+
+    cardBack.onload = function () {
+      ctx.drawImage(cardBack, cardX, cardY, cardWidth, cardHeight);
+    };
+  };
+  //remember to call onclick of card interior right
 
 
   function openCard() {
     canvas.addEventListener("click", function (ev) {
       if (ev.clientX > cardX
-        && ev.clientX < (cardX + cardFrontWidth)  //this doesn't work for some reason
+        && ev.clientX < (cardX + cardWidth)  //this doesn't work for some reason
         && ev.clientY > 50 && ev.clientY < 600) {
         console.log("clicked");
-        window.requestAnimationFrame(slideRight);
+        window.requestAnimationFrame(slideRight, 200);
       };
 
     }, false);
@@ -36,9 +46,22 @@ window.onload = function () {
   openCard();
 
   function slideRight() {
-    cardX += 50;
+    ctx.clearRect(0, 0, 700, 450);
     drawCardFront(); //cardX is incrementing, but not redrawing in correct place
-
+    cardX += 50;
     console.log(cardX, cardY);
   };
+
+
+  function drawInterior() {
+    ctx.rect(((canvas.width / 2) - cardWidth), cardY, cardWidth, cardHeight);
+    ctx.rect((canvas.width / 2), cardY, cardWidth, cardHeight);
+    ctx.strokeStyle = "#CCD";
+    ctx.fillStyle = "#FFE";
+    ctx.fill();
+    ctx.stroke();
+
+    //add text
+  };
+  drawInterior();
 };
