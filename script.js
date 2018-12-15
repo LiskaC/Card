984e7e5c4f2d09ab3d5f1818073b1a5bf64b42a4
Yelizaveta Crofts <yscrofts@hotmail.com>
2018-12-15 13:37:46 +0100

    front and inside drawn

--- script.js
+++ script.js
@@ -8,13 +8,17 @@ window.onload = function () {
   cardBack.src = "#"; //remember to upload
 
   cardHeight = 600;
-  cardWidth = cardHeight * (1921 / 2704);
-  cardX = ((canvas.width - cardWidth) / 2);
+  cardWidth = cardHeight * (1921 / 2704) /* w/h module*/;
+  cardFrontX = ((canvas.width - cardWidth) / 2);
   cardY = 50;
+  cardFrontInsideX = ((canvas.width / 2) - cardWidth);
+  cardBackInsideX = (canvas.width / 2);
 
+  card = {
+    open: "0"
+  }
   /*todo: 
   figure out why not visually sliding
-  add text to card interior
   get interior to appear onclick of front,
     then front interior click to stop working
   ditto back for onclick interior
@@ -23,74 +27,93 @@ window.onload = function () {
 
   function drawCardFront() {
 
-    cardFront.onload = function () {
-      ctx.drawImage(cardFront, cardX, cardY, cardWidth, cardHeight);
-    };
+    ctx.clearRect(0, 0, 1000, 700);
+    ctx.drawImage(cardFront, cardFrontX, cardY, cardWidth, cardHeight);
+    card.open = "0";
+    console.log(card.open);
   };
   drawCardFront();
 
 
   function drawInterior() {
+
+    ctx.clearRect(0, 0, 1000, 700);
     ctx.rect(((canvas.width / 2) - cardWidth), cardY, cardWidth, cardHeight);
     ctx.rect((canvas.width / 2), cardY, cardWidth, cardHeight);
     ctx.strokeStyle = "#CCD";
     ctx.fillStyle = "#FFE";
     ctx.fill();
     ctx.stroke();
+    card.open = "1";
 
-    //add text
+    function drawText() {
+      ctx.font = "12px serif";
+      ctx.fillStyle = '#000'
+      ctx.fillText("This be the card", 550, 100);
+      //add text
+    };
+    drawText();
   };
 
 
   function drawCardBack() {
 
+    ctx.clearRect(0, 0, 1000, 700);
     cardBack.onload = function () {
-      ctx.drawImage(cardBack, cardX, cardY, cardWidth, cardHeight);
+      ctx.drawImage(cardBack, cardFrontX, cardY, cardWidth, cardHeight);
     };
+    card.open = "2";
   };
   //remember to call onclick of card interior right
+  function openCard(ev) {
+    if (ev.clientX > cardFrontX
+      && ev.clientX < (cardFrontX + cardWidth)  //this doesn't work precisely for some reason
+      && ev.clientY > 50 && ev.clientY < 600) {
+      console.log("clicked");
+      drawInterior();
+      // window.requestAnimationFrame(slideRight, 200);
+    };
+  };
 
+  function flipToFront(ev) {
+    if (ev.clientX > cardFrontInsideX
+      && ev.clientX < (cardFrontInsideX + cardWidth)  //this doesn't work precisely for some reason
+      && ev.clientY > 50 && ev.clientY < 600) {
+      console.log("frontInside Clicked");
+      console.log(card.open);
+      drawCardFront();
+    };
+  };
 
-
-  canvas.addEventListener("click", function (ev) {
+  canvas.addEventListener("click", (ev) => {
 
     //make this work when front or back showing, but not interior    
-    function openCard() {
-      if (ev.clientX > cardX
-        && ev.clientX < (cardX + cardWidth)  //this doesn't work precisely for some reason
-        && ev.clientY > 50 && ev.clientY < 600) {
-        console.log("clicked");
-        drawInterior();
-        // window.requestAnimationFrame(slideRight, 200);
-      };
+
+
+    if (card.open == 0) {
+      openCard(ev);
+    } else if (card.open == 1) {
+      flipToFront(ev);
+      ctx.drawImage(cardFront, cardFrontX, cardY, cardWidth, cardHeight);
     };
-    openCard();
 
-    /*    
-        function toFront() {
-          if (within left of interior) {
-            ctx.clearRect(0, 0, 900, 700);
-            drawCardFront();
-          };
-        };
-    
-        function toBack() {
-          if (within right of interior) {
-            ctx.clearRect(0, 0, 900, 700);
-            drawCardBack();
-          };
-        };
-        */
+    /*   function toBack() {
+         if (within right of interior) {
+       ctx.clearRect(0, 0, 900, 700);
+       drawCardBack();
+     };
+   };*/
 
-  }, false);
+  });
 
   /* this is not working
 
   function slideRight() {
     // ctx.clearRect(0, 0, 700, 450);
-    drawCardFront(); //cardX is incrementing, but not redrawing in correct place
-    cardX += 50;
-    console.log(cardX, cardY);
+    drawCardFront(); //cardFrontX is incrementing, but not redrawing in correct place
+    cardFrontX += 50;
+    console.log(cardFrontX, cardY
+  );
   };
   */
 
